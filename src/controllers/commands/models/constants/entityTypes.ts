3d36a4bf643f8ff3a47284b341d4a417eecba030
Khaled Ras Guerriche <54240938+Khaled-Pr@users.noreply.github.com>
2021-02-20 21:41:18 -0600

    entity types for employee classification

---
+++ src/controllers/commands/models/constants/entityTypes.ts
@@ -0,0 +1,15 @@
+export enum EmployeeClassification {
+	NotDefined = -1,
+    GeneralManager = 300,
+    ShiftManager = 200,
+	Cashier = 100	
+	
+}
+
+export enum EmployeeClassificationLabel {
+	NotDefined = "Not Selected",
+    GeneralManager = "General Manager",
+    ShiftManager = "Shift Manager",
+	Cashier = "Cashier"	
+	
+}
